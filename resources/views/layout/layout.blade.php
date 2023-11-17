4a2f7002c8b56173e8c694961878ca133a038b3c
EmanuelePasquale <emanuelep.papale@gmail.com>
2023-11-17 19:51:59 +0100

    molti cambiamenti

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>ZenBlog</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500&family=Inter:wght@400;500&family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+   @vite(['resources\css\app.css', 'resources\js\app.js'])
+</head>
+
+<body>
+    <x-navbar/>
+
+  @if(session('success'))
+  <div class="alert alert-success" role="alert">
+      {{session('success')}}
+  </div>
+  @endif
+  @if(session('danger'))
+  <div class="alert alert-danger" role="alert">
+      {{session('danger')}}
+  </div>
+  @endif
+
+{{$slot}}
+
+<x-footer/>
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+</body>
+
+</html>
