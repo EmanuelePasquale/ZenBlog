366ae85e456dca2e808797b6933107a9ccb8fe11
EmanuelePasquale <emanuelep.papale@gmail.com>
2023-11-17 18:41:30 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+import 'bootstrap';
+import './main.js';
+import '/vendor/bootstrap/js/bootstrap.bundle.min.js';
+import '/vendor/swiper/swiper-bundle.min.js';
+import '/vendor/glightbox/js/glightbox.min.js';
+import '/vendor/php-email-form/validate.js';
